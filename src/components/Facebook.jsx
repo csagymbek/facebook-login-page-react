09e0c62477b3d3c8dd50b6d1f70d762fe6b435dd
csagymbek <csagymbek@gmail.com>
2020-12-14 14:25:44 -0800

    finished version

---
+++ src/components/Facebook.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
+
+export default class Facebook extends Component {
+  state = { isLoggedIn: false, userId: "", name: "", email: "", picture: "" };
+
+  componentClicked = () => {
+    console.log("clicked");
+  };
+
+  responseFacebook = (response) => {
+    // console.log(response);
+    this.setState({
+      isLoggedIn: true,
+      userId: response.userID,
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url,
+    });
+  };
+
+  render() {
+    let fbContent;
+
+    if (this.state.isLoggedIn) {
+      fbContent = (
+        <div
+          style={{
+            width: "400px",
+            margin: "auto",
+            background: "#f4f4f4",
+            padding: "20px",
+          }}
+        >
+          <img src={this.state.picture} alt={this.state.name} />
+          <h2>Welcome {this.state.name}</h2>
+          Email: {this.state.email}
+        </div>
+      );
+    } else {
+      fbContent = (
+        <FacebookLogin
+          appId="834708017327352"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+      );
+    }
+
+    return <div>{fbContent}</div>;
+  }
+}
